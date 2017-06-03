7e0433b0a7e98e17f9282943dc0dc2b0a49b8d9a
Yuri Dagot <yuri@Yuris-MacBook-Pro.local>
2017-06-03 18:53:34 +0100

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by Yuri Dagot on 03/06/2017.
+//  Copyright © 2017 Yuri Dagot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
